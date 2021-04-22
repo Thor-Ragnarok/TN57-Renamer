bf04bfe6aa76d585c742a412056a89c02516338a
Thor⚡ <82438870+Thor-Ragnarok@users.noreply.github.com>
2021-04-22 11:48:41 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,7 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact ⭕️", url="https://t.me/ML_SupportGroup")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⛑️ Contact ⛑️", url="https://t.me/TN57_BotzSupport")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
